42a7dac13da5d6c2d3d8cf0e44b32f1017cf22cf
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-08-17 18:50:49 -0300

    Requisito 6: Concluido

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT 
+	MIN(pl.valor_plano) AS faturamento_minimo,  
+	MAX(pl.valor_plano) AS faturamento_maximo,
+	ROUND(AVG(pl.valor_plano), 2) AS faturamento_medio,
+	ROUND(SUM(pl.valor_plano), 2) AS faturamento_total
+    FROM SpotifyClone.planos AS pl
+    INNER JOIN SpotifyClone.usuarios AS us
+    ON us.plano_id=pl.plano_id;
